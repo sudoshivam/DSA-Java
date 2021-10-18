e0c113a125400dd4887d18d938c38ec502da8551
Shivam Yadav <sy76076@gmail.com>
2021-10-18 21:49:41 +0530

    Add Print Armstrong Number

--- src/com/sudoshivam/Armstrong.java
+++ src/com/sudoshivam/Armstrong.java
@@ -1,17 +1,22 @@
-// Check if a number is Armstrong number
+// Print all Armstrong number of given digit
 package com.sudoshivam;
 
 public class Armstrong {
     public static void main (String[] args) {
-        int num = 153; // Enter a number or take user input
-        System.out.println(checkArmstrong(num));
+        /* For 3 digit number we take 100 to 999 */
+        for (int i = 100; i <= 999; i++) {
+            if (checkArmstrong(i)) {
+                System.out.println(i);
+            }
+        }
     }
     static boolean checkArmstrong(int num) {
         int original = num; // Save it for comparing later
         int sum = 0;
         while (num > 0) {
-            int rem = num % 10; // Take the last digit
-            num /= 10;
+            int rem = num % 10; // Take last digit in rem
+            num /= 10; // Remove last digit from num
+            // We do sum of 'cube' of every digit because no. of digits in num = 3
             sum += (rem*rem*rem);
         }
         return original == sum;
